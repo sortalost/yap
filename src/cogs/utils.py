c6c8d7473a494a5da20596b15c261deebec9bdf9
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2024-07-28 16:53:49 +0530

    yey

--- src/cogs/utils.py
+++ src/cogs/utils.py
@@ -2,9 +2,9 @@ import requests as r
 import json
 import os
 
-jsfile="https://gist.github.com/v1s1t0r999/e209d7a7c847570b84a2803f22071819/"
+jsfile="https://gist.github.com/v1s1t0r999/c505d338345f02e3036f851346f8a7a4"
 url="https://api.github.com/gists/"
-gist="e209d7a7c847570b84a2803f22071819"
+gist="c505d338345f02e3036f851346f8a7a4"
 token=os.getenv("token") 
 headers={'Authorization':'token {}'.format(token)}
 params={'scope':'gist'}
@@ -38,10 +38,10 @@ def addpost(name, date, time, content, img):
             "description":f"splat dump - {num}",
             "public":True,
             "files":{
-                "json":{"content": json.dumps(con)}
+                "data.json":{"content": json.dumps(con)}
                 }
         }
-    res=r.patch(url+gist, headers=headers, params=params, data=json.dumps(payload))
+    res=r.patch(url+gist, headers=headers, params=params, data=json.dumps(payload,indent=4))
 
     print(res.text)
     return str(num)
